4ae611ba54aee4944b0dccdffb99ccba0093c1a6
sophsuan <47379998+sophsuan@users.noreply.github.com>
2021-01-23 11:00:53 -0800

    connected index.html and restructured

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + "/../public"));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
